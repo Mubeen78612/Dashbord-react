739f7330cacd4509f625d569a89208177fa774bc
mubeen malik <mubeenmalik1211@gmail.com>
2023-01-23 01:51:43 +0000

    add folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./assets/css/main.css"
 export const App = () => {
   return (
     <div>App is Running</div>
---
+++ src/routes/routers.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const routers = () => {
+  return (
+    <div>routers</div>
+  )
+}
